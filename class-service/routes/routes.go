370413470ef4a0882129b877a9f365571fd69de6
melvinjoshuaputra-hub <melvinjoshuaputra@gmail.com>
2026-05-26 21:46:06 +0700

    push class service

---
+++ class-service/routes/routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"phase3/finalproject/class_service-melvin/handlers"
+	"phase3/finalproject/class_service-melvin/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(r *gin.Engine) {
+
+	auth := r.Group("/")
+	auth.Use(middleware.JWTMiddleware())
+
+	auth.POST("/classes", handlers.CreateClass)
+	auth.GET("/classes", handlers.GetAllClasses)
+	auth.POST("/classes/join", handlers.JoinClass)
+}
